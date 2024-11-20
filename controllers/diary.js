08bb05b453230242d5287ac7e90a9e675b2d55f4
ashwith10 <143590935+ashwith10@users.noreply.github.com>
2024-11-20 02:55:48 -0600

    work is in process

--- controllers/diary.js
+++ controllers/diary.js
@@ -123,5 +123,81 @@ exports.diary_update_put = async function(req, res) {
         res.send(`{"error": ${err}: Update for id ${req.params.id} failed}`);
     }
 };
+
+// Handle diary delete on DELETE.
+exports.diary_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await diary.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+
+    // Handle a show one view with id specified by query
+exports.diary_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await diary.findById( req.query.id)
+    res.render('diarydetail',
+    { title: 'diary Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle building the view for creating a diary.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.diary_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('diarycreate', { title: 'diary Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+
+    // Handle building the view for updating a diary.
+// query provides the id
+exports.diary_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await diary.findById(req.query.id)
+    res.render('diaryupdate', { title: 'diary Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle a delete one view with id from query
+exports.diary_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await diary.findById(req.query.id)
+    res.render('diarydelete', { title: 'diary Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+   
+    
+    
+    
  
    
+
+    
--- routes/diary.js
+++ routes/diary.js
@@ -4,6 +4,20 @@ var diary_controllers = require('../controllers/diary');
  
 /* GET home page. */
 router.get('/',diary_controllers.diary_view_all_Page);
+
+/* GET detail diary page */
+router.get('/detail',diary_controllers.diary_view_one_Page);
+
+/* GET create diary page */
+router.get('/create', diary_controllers.diary_create_Page);
+
+/* GET create update page */
+router.get('/update', diary_controllers.diary_update_Page);
+
+/* GET delete diary page */
+router.get('/delete', diary_controllers.diary_delete_Page);
+
+
  
  
 router.get('/create',diary_controllers.diary_create_post);
